1daaa3b290f4f5b5ef15238854b4d2dea4959db7
Ayush <ayushgore21@gmail.com>
2026-05-20 16:31:09 +0530

    fix doctor uuid dropdown issue

--- frontend/src/components/BookingForm.tsx
+++ frontend/src/components/BookingForm.tsx
@@ -44,6 +44,8 @@ export default function BookingForm({ initialDeptId, initialDocId }: { initialDe
   // Handle department shift
   const handleDocChange = (docId: string) => {
     const realDoctor = doctors.find((d) => d.id === docId);
+    console.log("REAL DOCTOR ID:", realDoctor?.id);
+    console.log("REAL DOCTOR PUBLIC ID:", realDoctor?.publicId);
 
     setSelectedDocId(docId);
 
